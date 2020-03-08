75a6656142c8ed4b3eecdd7c323118304e367e96
nnrogers515 <nnrogers515@gmail.com>
2020-03-08 17:01:23 -0500

    Updated TestMain.kt for Server test

--- src/test/kotlin/TestMain.kt
+++ src/test/kotlin/TestMain.kt
@@ -1,9 +1,18 @@
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 import io.ktor.server.testing.withTestApplication
+import io.ktor.application.Application
+import io.ktor.http.HttpMethod.Companion.Get
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.handleRequest
 
 class TestMain : StringSpec({
     "should retrieve root path properly" {
-        withTestApplication {  }
+        withTestApplication(Application::adder) {
+            handleRequest(Get, "/").apply {
+                response.status() shouldBe HttpStatusCode.OK
+                response.content shouldBe "Hello, World"
+            }
+        }
     }
 })
